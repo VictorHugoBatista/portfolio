3470d969faa2c56e6360d891d921f7643530eef2
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-03 03:28:32 -0300

    Add responsivity to the banner

---
+++ src/components/banner-ascii-responsive.jsx
@@ -0,0 +1,62 @@
+import { useEffect, useState } from "react";
+
+import { colossal, cyberlarge, stickLetters, mini, useAsciiText } from "react-ascii-text";
+
+const fontsByResolution = [
+  {
+    resolution: 1200,
+    font: colossal,
+  },
+  {
+    resolution: 992,
+    font: cyberlarge,
+  },
+  {
+    resolution: 768,
+    font: stickLetters,
+  },
+  {
+    resolution: 0,
+    font: mini,
+  },
+];
+
+const getWindowDimensions = () => {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+};
+
+const getFontByResolution = (fontsByResolution, windowDimensions) => {
+  for (const key in fontsByResolution) {
+    const fontByResolution = fontsByResolution[key];
+    console.log(windowDimensions.width, fontByResolution);
+    if (windowDimensions.width < fontByResolution.resolution) {
+      continue;
+    }
+    return fontByResolution.font;
+  }
+}
+
+function BannerAsciiResponsive(props) {
+  const [text] = useState(props.texts);
+  const windowDimensions = getWindowDimensions();
+  const font = getFontByResolution(fontsByResolution, windowDimensions);
+  const asciiTextRef = useAsciiText({
+    animationCharacters: "▒░█",
+    animationCharacterSpacing: 1,
+    animationDelay: 2000,
+    animationDirection: "horizontal",
+    animationInterval: 100,
+    animationLoop: true,
+    animationSpeed: 3,
+    font: font,
+    text,
+  });
+
+    return <pre ref={asciiTextRef} />;
+};
+
+export default BannerAsciiResponsive;
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,24 +1,10 @@
-import { useState } from "react";
-import { colossal, useAsciiText } from "react-ascii-text";
-
 import '../App.css';
 
 import Monitor from '../layout/Monitor';
 
-function Home() {
-  const [text] = useState(["Tecnologia", "Software", "Solucao", "Qualidade", "Fullstack"]);
-  const asciiTextRef = useAsciiText({
-    animationCharacters: "▒░█",
-    animationCharacterSpacing: 1,
-    animationDelay: 2000,
-    animationDirection: "horizontal",
-    animationInterval: 100,
-    animationLoop: true,
-    animationSpeed: 3,
-    font: colossal,
-    text,
-  });
+import BannerAsciiResponsive from "../components/banner-ascii-responsive";
 
+function Home() {
   return (
     <Monitor>
       <header className="monitor-screen-title">
@@ -26,18 +12,18 @@ function Home() {
       </header>
       <section className="monitor-screen-content">
         <div className="monitor-screen-jumbotron">
-          <pre ref={asciiTextRef} />
+          <BannerAsciiResponsive texts={["Tecnologia", "Software", "Solucao", "Qualidade", "Fullstack"]} />
         </div>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis magni ducimus vero voluptate tempore eum ex deserunt accusamus, harum ab temporibus pariatur cum officiis, ullam minima voluptatem doloremque id dolorum!</p>
-          <p>Voluptas iste magni aliquam voluptatem id odio dolores, excepturi, autem earum architecto, dolor beatae! Atque ipsam nam nisi, commodi repudiandae rerum, expedita fuga ad, maxime inventore explicabo tempore tempora quas.</p>
-          <p>Ex dolorum incidunt commodi at repellat suscipit! Maxime necessitatibus quidem repellendus vitae tenetur sunt explicabo? Hic quo necessitatibus voluptatum, autem sapiente ea voluptas. Inventore, asperiores deleniti veniam necessitatibus pariatur nihil.</p>
-          <p>Pariatur iusto voluptates eveniet ullam delectus? Rem tempore optio minima officia, hic veritatis neque totam deleniti soluta doloribus nisi laudantium? Magni recusandae consequuntur officia, quaerat harum porro mollitia officiis quisquam.</p>
-          <p>Saepe, ipsum maxime pariatur nisi ab molestiae omnis similique dolorem debitis nobis! Eveniet non expedita cum commodi doloribus quo a temporibus, reprehenderit architecto at, alias, omnis eos minus debitis sapiente.</p>
-          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis magni ducimus vero voluptate tempore eum ex deserunt accusamus, harum ab temporibus pariatur cum officiis, ullam minima voluptatem doloremque id dolorum!</p>
-          <p>Voluptas iste magni aliquam voluptatem id odio dolores, excepturi, autem earum architecto, dolor beatae! Atque ipsam nam nisi, commodi repudiandae rerum, expedita fuga ad, maxime inventore explicabo tempore tempora quas.</p>
-          <p>Ex dolorum incidunt commodi at repellat suscipit! Maxime necessitatibus quidem repellendus vitae tenetur sunt explicabo? Hic quo necessitatibus voluptatum, autem sapiente ea voluptas. Inventore, asperiores deleniti veniam necessitatibus pariatur nihil.</p>
-          <p>Pariatur iusto voluptates eveniet ullam delectus? Rem tempore optio minima officia, hic veritatis neque totam deleniti soluta doloribus nisi laudantium? Magni recusandae consequuntur officia, quaerat harum porro mollitia officiis quisquam.</p>
-          <p>Saepe, ipsum maxime pariatur nisi ab molestiae omnis similique dolorem debitis nobis! Eveniet non expedita cum commodi doloribus quo a temporibus, reprehenderit architecto at, alias, omnis eos minus debitis sapiente.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis magni ducimus vero voluptate tempore eum ex deserunt accusamus, harum ab temporibus pariatur cum officiis, ullam minima voluptatem doloremque id dolorum!</p>
+        <p>Voluptas iste magni aliquam voluptatem id odio dolores, excepturi, autem earum architecto, dolor beatae! Atque ipsam nam nisi, commodi repudiandae rerum, expedita fuga ad, maxime inventore explicabo tempore tempora quas.</p>
+        <p>Ex dolorum incidunt commodi at repellat suscipit! Maxime necessitatibus quidem repellendus vitae tenetur sunt explicabo? Hic quo necessitatibus voluptatum, autem sapiente ea voluptas. Inventore, asperiores deleniti veniam necessitatibus pariatur nihil.</p>
+        <p>Pariatur iusto voluptates eveniet ullam delectus? Rem tempore optio minima officia, hic veritatis neque totam deleniti soluta doloribus nisi laudantium? Magni recusandae consequuntur officia, quaerat harum porro mollitia officiis quisquam.</p>
+        <p>Saepe, ipsum maxime pariatur nisi ab molestiae omnis similique dolorem debitis nobis! Eveniet non expedita cum commodi doloribus quo a temporibus, reprehenderit architecto at, alias, omnis eos minus debitis sapiente.</p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis magni ducimus vero voluptate tempore eum ex deserunt accusamus, harum ab temporibus pariatur cum officiis, ullam minima voluptatem doloremque id dolorum!</p>
+        <p>Voluptas iste magni aliquam voluptatem id odio dolores, excepturi, autem earum architecto, dolor beatae! Atque ipsam nam nisi, commodi repudiandae rerum, expedita fuga ad, maxime inventore explicabo tempore tempora quas.</p>
+        <p>Ex dolorum incidunt commodi at repellat suscipit! Maxime necessitatibus quidem repellendus vitae tenetur sunt explicabo? Hic quo necessitatibus voluptatum, autem sapiente ea voluptas. Inventore, asperiores deleniti veniam necessitatibus pariatur nihil.</p>
+        <p>Pariatur iusto voluptates eveniet ullam delectus? Rem tempore optio minima officia, hic veritatis neque totam deleniti soluta doloribus nisi laudantium? Magni recusandae consequuntur officia, quaerat harum porro mollitia officiis quisquam.</p>
+        <p>Saepe, ipsum maxime pariatur nisi ab molestiae omnis similique dolorem debitis nobis! Eveniet non expedita cum commodi doloribus quo a temporibus, reprehenderit architecto at, alias, omnis eos minus debitis sapiente.</p>
       </section>
       <footer className="monitor-screen-footer">
         <ul>
